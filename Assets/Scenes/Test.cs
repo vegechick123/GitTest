df23697c9768d309a0dffef99bdfdc8ce3e4a483
vegechick123 <destiny173@163.com>
2020-12-21 13:22:39 +0800

    Update Test.cs

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
